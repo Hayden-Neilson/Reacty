01550422dd034de50d6c05597ab01e192b9bdabc
Hayden-Neilson <haydenneilson5@gmail.com>
2020-07-27 17:23:31 -0600

    removed a bug incode

--- src/App.js
+++ src/App.js
@@ -19,23 +19,24 @@ class App extends Component {
       persons: [
         { name: "Max", age: 28 },
         { name: "george", age: 54 },
-        { name: "betty", age: 37 };
+        { name: "betty", age: 37 },
     })
-  }
+  };
 
-}
 
-render() {
-  return (
-    <div className="App">
-      <h1>Hi Im a react app</h1>
-      <button onClick={this.switchNameHandler}>Switch Name</button>;
-      <p>This is really working</p>;
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>   
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-    </div>
-  );
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi Im a react app</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>;
+        <p>This is really working</p>;
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  };
 };
 
-export default App;
+  export default App;
