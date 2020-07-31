ea1fa3a804db708853604bef467dd5380766776e
Hayden-Neilson <haydenneilson5@gmail.com>
2020-07-30 19:55:48 -0600

    put work in hooks at the bottom for reference

--- src/App.js
+++ src/App.js
@@ -47,3 +47,44 @@ class App extends Component {
 }
 
 export default App;
+
+// const app = (props) => {
+//   const [personsState, setPersonsState] = {
+//     persons: [
+//       { name: "Max", age: 28 },
+//       { name: "george", age: 54 },
+//       { name: "betty", age: 37 },
+//     ],
+//   };
+
+//   const switchNameHandler = () => {
+//     // console.log("was clicked");
+//     setPersonsState({
+//       persons: [
+//         { name: "Maxy", age: 28 },
+//         { name: "george", age: 54 },
+//         { name: "betty", age: 89 },
+//       ],
+//     });
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Hi Im a react app</h1>
+//       <button onClick={switchNameHandler}>Switch Name</button>;
+//       <p>This is really working</p>;
+//       <Person
+//         name={personsState.persons[0].name}
+//         age={personsState.persons[0].age}
+//       />
+//       <Person
+//         name={personsState.persons[1].name}
+//         age={personsState.persons[1].age}
+//       />
+//       <Person
+//         name={personsState.persons[2].name}
+//         age={personsState.persons[2].age}
+//       />
+//     </div>
+//   );
+// };
