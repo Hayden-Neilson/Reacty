313b4d789cd3431b2215a4d9e393d3710f107906
Hayden-Neilson <haydenneilson5@gmail.com>
2020-08-01 15:23:47 -0600

    add styes to person

--- src/Person/person.js
+++ src/Person/person.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 
+import "./Person.scss";
 const Person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p>
         I'm {props.name} and I am {props.age} years old!
       </p>
